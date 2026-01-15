d0c00a74d3e36e5c476dd1718655dd0ce788c042
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-15 13:24:27 -0500

    Create proofs-approved.php

---
+++ src/Email/Templates/proofs-approved.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'subject' => 'Proofs approved for "{{project_title}}"',
+    'body' => "Hello {{client_name}},\n\n"
+        . "Thanks for approving the proofs for \"{{project_title}}\". Your selections have been received and the editing process will begin shortly.\n\n"
+        . "We'll notify you when the final gallery is ready.\n\n"
+        . "Thanks,\nThe Aperture Pro Team",
+];
