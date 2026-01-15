02acfa47785157c404a595efcfb98bbf1fcd0f38
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-15 13:15:04 -0500

    Update StorageFactory.php

--- src/Storage/StorageFactory.php
+++ src/Storage/StorageFactory.php
@@ -3,16 +3,36 @@
 namespace AperturePro\Storage;
 
 use AperturePro\Config\Config;
+use AperturePro\Helpers\Logger;
 
-class StorageFactory {
-    public static function make(): StorageInterface {
-        $driver = Config::get('storage.driver');
+/**
+ * StorageFactory
+ *
+ * Returns an instance of StorageInterface based on plugin configuration.
+ */
+class StorageFactory
+{
+    /**
+     * Make a storage driver instance based on configuration.
+     *
+     * @return StorageInterface
+     * @throws \RuntimeException if driver cannot be instantiated
+     */
+    public static function make(): StorageInterface
+    {
+        $config = Config::all();
+        $driver = $config['storage']['driver'] ?? 'local';
 
-        return match ($driver) {
-            'local'     => new LocalStorage(),
-            'imagekit'  => new ImageKitStorage(),
-            'cloudinary'=> new CloudinaryStorage(),
-            default     => new LocalStorage(),
-        };
+        switch (strtolower($driver)) {
+            case 'local':
+                return new LocalStorage($config['storage'] ?? []);
+            case 'imagekit':
+                return new ImageKitStorage($config['storage'] ?? []);
+            case 'cloudinary':
+                return new CloudinaryStorage($config['storage'] ?? []);
+            default:
+                Logger::log('error', 'storage_factory', 'Unknown storage driver requested', ['driver' => $driver]);
+                throw new \RuntimeException("Unknown storage driver: {$driver}");
+        }
     }
 }
