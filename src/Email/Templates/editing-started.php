5a65f835c3e38036fdf32be05bc8e2e17397be34
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-15 13:24:48 -0500

    Create editing-started.php

---
+++ src/Email/Templates/editing-started.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'subject' => 'Editing started for "{{project_title}}"',
+    'body' => "Hello {{client_name}},\n\n"
+        . "Your photographer has started editing the selected images for \"{{project_title}}\".\n\n"
+        . "We will notify you when the final gallery is ready for delivery.\n\n"
+        . "Thanks,\nThe Aperture Pro Team",
+];
